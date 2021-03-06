440a8b8e19d05062817a10fa34e1253fab225a5a
Макаров Павел Владиславович <makarovpv.25@sch239.net>
2021-03-06 19:26:44 +0300

    working on AI

---
+++ scripts/Camicadze.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camicadze : MonoBehaviour
+{
+    public float speed = 1f;
+    private GameObject player;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targ = Target();
+        rb.velocity = targ / targ.magnitude * speed;
+    }
+
+    Vector3 Target()
+    {
+        Vector3 targ = Vector3.zero;
+
+        targ = player.transform.position;
+
+        return targ;
+    }
+}
