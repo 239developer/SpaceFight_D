2807b6cdded3140661ba58f7d32fbe143d40a1c0
Макаров Павел Владиславович <makarovpv.25@sch239.net>
2021-03-16 15:01:38 +0300

    added working rocketship enemy

---
+++ scripts/Rocket.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public GameObject player, particles;
+    public static float speed = 5f;
+    public static float degreesPerSecond = 60f;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+        transform.position += new Vector3(0f, 0f, playerMovement.speedForv * Time.deltaTime);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(Vector3.down, player.transform.position - transform.position), degreesPerSecond * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Enemy")
+        {
+            if(other.name == "Player")
+                playerMovement.health -= playerMovement.rocketDmg;
+            var x = GameObject.Instantiate(particles, transform.position, transform.rotation);
+            Destroy(x, 2.5f);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ scripts/Rocketship.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocketship : MonoBehaviour
+{
+    public Vector3 rocketBias = new Vector3(0f, 0.55f, 0f);
+    public static float reloadTime = 3f;
+    public GameObject rocket, particles;
+    private bool isFiring;
+    private float lastShotTime = 0f;
+    private GameObject laser;
+
+    void Fire()
+    {
+        if(Time.time - lastShotTime >= reloadTime)
+        {
+            lastShotTime = Time.time;
+            var x = GameObject.Instantiate(rocket, transform.position + rocketBias, rocket.transform.rotation);
+            Destroy(x, 5f);
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(0f, 0f, -playerMovement.speedForv * Time.deltaTime);
+        Fire();
+    }
+}
