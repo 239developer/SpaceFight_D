a2de2b4c8457951028825eceb126eb1f826c522b
Agnieszka Makarova <hawthorn7@yande.ru>
2021-04-07 19:57:16 +0300

    fixed ugly slider in settings

--- scripts/menu/settings.cs
+++ scripts/menu/settings.cs
@@ -5,20 +5,23 @@ using UnityEngine.UI;
 
 public class settings : MonoBehaviour
 {
-    public Slider music;
+    public Slider music, musicClip;
+    private static int startClip;
 
     void Start()
     {
+        startClip = audioController.clip;
         music.value = audioController.musicVolume;
+        musicClip.value = audioController.clip;
     }
 
     void Update()
     {
         audioController.musicVolume = music.value;
-    }
-
-    void ChangeMusic()
-    {
-
+        audioController.clip = (int)musicClip.value;
+        if(startClip != audioController.clip)
+        {
+            audioController.musicTime = 0f;
+        }
     }
 }
