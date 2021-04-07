1f080a2775024d01a13fe7c9e87e2611ac7c0c2a
PG805-lab <72620110+PG805-lab@users.noreply.github.com>
2021-04-07 20:00:36 +0300

    Add files via upload

---
+++ scripts/Globals.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    public static bool die = false;
+    public static bool pause = false;
+    public static bool no = false;
+}
